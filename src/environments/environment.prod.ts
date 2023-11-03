68bdd4ba1d92c0e3db349c02351dc148620d6a37
alexim39 <alexim39@yahoo.com>
2023-11-03 17:30:33 +0100

    changed endpoint in env folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   title: "Production Environment Variables",
-  apiUrl: 'https://async-training-api.onrender.com'
+  apiUrl: 'https://asynctrainingapi4-x0bw8fkh.b4a.run'
 };
